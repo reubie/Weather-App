d51c3e809904ed15a9dbbfda0f1c5fa1bc4db92c
reubie <r.nguyo27@gmail.com>
2022-03-23 11:04:58 +0300

    create mvvm packages

--- app/src/main/java/com/example/weatherapplication/view/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.weatherapplication
+package com.example.weatherapplication.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.weatherapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
